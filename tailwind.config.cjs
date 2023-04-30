6714cd06d4d3463c6f7087aa714dfac91308e58c
Danar Widi <widiu7omo@gmail.com>
2023-04-30 19:55:39 +0800

    update content tailwindcss

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  
   content: [
     "./renderer/**/*.{vue,js,ts,jsx,tsx}",
     "./pages/**/*.{vue,js,ts,jsx,tsx}",
+    "./core/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
